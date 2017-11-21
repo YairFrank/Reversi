b8be892bd8e57588c52b64df619e9c1b5b52cb69
orlinle <leahorlin@gmail.com>
2017-11-21 17:36:08 +0200

    added test file

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+ * helloWorld.cpp
+ *
+ *  Created on: Nov 21, 2017
+ *      Author: leah
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello world." << endl;
+	return 0;
+}
+
+
+
+
